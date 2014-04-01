70fc6f3c3fd0276b4b623b02a1584b51882d84b7
lance turner <lance@aeonblu.com>
2014-04-01 15:22:27 -0400

    Correct bug preventing display of more than one dynamic sidebar in footer. #2

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 <!-- Footer -->
 <footer class="row">
 
-<?php if ( dynamic_sidebar('Sidebar Footer One') || dynamic_sidebar('Sidebar Footer Two') || dynamic_sidebar('Sidebar Footer Three') || dynamic_sidebar('Sidebar Footer Four')  ) : else : ?>
+<?php if ( dynamic_sidebar('Sidebar Footer One') + dynamic_sidebar('Sidebar Footer Two') + dynamic_sidebar('Sidebar Footer Three') + dynamic_sidebar('Sidebar Footer Four')  ) : else : ?>
 
 <div class="large-12 medium-12 columns">
 	<ul class="inline-list">
